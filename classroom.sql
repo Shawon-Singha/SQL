8beffafc47be297f21aa8c85bc3ca363878c8681
Shawon-Singha <shawonsingha10@gmail.com>
2024-04-02 23:29:39 +0600

    update

---
+++ classroom.sql
@@ -0,0 +1,17 @@
+create database college;
+
+use college;
+
+create table student(
+  id int primary key,
+  name varchar(50),
+  age int not null
+);
+
+insert into student values(1,"shawon",22);
+insert into student values(2,"Anik",23);
+
+select * from student;
+
+
+
